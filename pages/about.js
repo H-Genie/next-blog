3493c02cc00c3534f5d502236a03850f99b1ff26
h-genie <ses2000hj@hanmail.net>
2024-06-24 21:16:05 +0900

    159. Next 헤드 컴포넌트

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,12 @@
+import Head from "next/head"
+
 export default function AboutPage() {
   console.log("[AboutPage] render")
   return (
     <>
+      <Head>
+        <title>About - My Blog</title>
+      </Head>
       <main>
         <h1>About</h1>
       </main>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Head from "next/head"
+
 export default function HomePage() {
   console.log("[HomePage] render")
   return (
     <>
+      <Head>
+        <title>My Blog</title>
+      </Head>
       <main>
         <h1>My Blog</h1>
       </main>
