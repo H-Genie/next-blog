fa9eaa97e68b3ab863803c37bae5f26ccc75c7ed
h-genie <ses2000hj@hanmail.net>
2024-07-09 21:10:44 +0900

    174. ThemeSitch 컴포넌트

---
+++ components/ThemeSwitch.js
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export default function ThemeSwitch() {
+  const [darkMode, setDarkMode] = useState(false)
+
+  console.log("🚀 ~ ThemeSwitch ~ darkMode:", darkMode)
+  const text = darkMode ? "Light Mode" : "Dark Mode"
+  return (
+    <>
+      <button onClick={() => setDarkMode(!darkMode)}>{text}</button>
+      <style jsx>{`
+        button {
+          background: none;
+          border: none;
+          cursor: pointer;
+        }
+      `}</style>
+    </>
+  )
+}
